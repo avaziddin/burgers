b462e1982c59182a4a99c125591a570419e3ed6d
avaziddin <avazsharifov11@gmail.com>
2024-10-08 20:12:51 +0500

    second commit

--- components/modal.tsx
+++ components/modal.tsx
@@ -2,7 +2,7 @@
 export default function Modal() {
     return (
         <div className="w-full h-screen bg-[rgba(0,0,0,0.8)] absolute top-1">
-            aaaa
+            aaaaaaaaaaa
         </div>
     )
 }
