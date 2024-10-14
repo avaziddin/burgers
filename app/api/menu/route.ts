0b0dda55b4967a86f8310526f18adbc0b3ed6745
Yunus <ibadovunus55@gmail.com>
2024-10-14 16:30:23 +0500

    avaz gay

--- app/api/menu/route.ts
+++ app/api/menu/route.ts
@@ -1,8 +1,18 @@
 import { NextRequest, NextResponse } from "@/node_modules/next/server";
 
-export const GET = async (res: NextResponse , req:NextRequest) => {
+export const GET = async () => {
+    try {
+        const client = await clientPromise;
+        const db = client.db('mydatabase');
 
-}   
+        const menu = await db.collection("menu").find().toArray();
+
+        return NextResponse.json({ success: true, data: menu, message: "success" });
+
+    } catch (e: any) {
+        return NextResponse.json({ success: false, message: e.message }, { status: 500 });
+    }
+}; 
 
 export const POST = async (res: NextResponse , req:NextRequest) => {
 
